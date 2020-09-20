818e0cb57bd0ec94870494ca925b5227fcf6a093
BrettS89 <blsodie@gmail.com>
2020-09-20 19:42:05 -0400

    fixing uri

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   port: 5000,
-  URI: 'http://localhost:4000/api',
-  SOCKET_URI: 'http://localhost:4000',
-  // URI: 'https://litam-api-staging.herokuapp.com/api',
+  // URI: 'http://localhost:4000/api',
+  URI: 'https://litam-api-staging.herokuapp.com/api',
+  // SOCKET_URI: 'http://localhost:4000',
+  SOCKET_URI: 'https://litam-api-staging.herokuapp.com',
 };
