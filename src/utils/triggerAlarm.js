750d92446af1704ff392afad4e1379c4ca276266
BrettS89 <blsodie@gmail.com>
2020-09-26 12:24:49 -0400

    fixing missing arg

--- src/utils/triggerAlarm.js
+++ src/utils/triggerAlarm.js
@@ -9,21 +9,17 @@ module.exports = async (eventEmitter, alarm) => {
   const date = getIsoDate();
   if (alarm.rang.includes(date)) return null;
   console.log('RING RING RING');
-  // try {
+  
   const { data: { alarmMessage, alarmId, removeAlarm } } = await getAlarmMessage(alarm._id);
   audio.downloadAndPlay(alarmMessage.song.audio);
-  //   console.log('success!');
 
   if (removeAlarm) {
     const alarmsClone = _.cloneDeep(state.alarms);
     const filteredAlarmsClone = alarmsClone.filter(a => a._id !== alarmId);
     state.alarms = filteredAlarmsClone;
     eventEmitter.clear();
-    filteredAlarmsClone.forEach(a => setAlarmInEmitter(a));
+    filteredAlarmsClone.forEach(a => setAlarmInEmitter(a, eventEmitter));
   }
 
-  // } catch(e) {
-  //   console.log('triggerAlarm error', e)
-  // }
   return true;
 };
